3200ee41c113c8d75605b433c4cf1e325ccfeaea
Daniel Orol <danielhorol@gmail.com>
2017-03-30 17:34:39 -0400

    Ramp function input instead of step

--- cmd_publisher.py
+++ cmd_publisher.py
@@ -5,8 +5,18 @@ def talker():
     pub = rospy.Publisher('/cmd_publish', String, queue_size=10)
     rospy.init_node('cmd_publish', anonymous=True)
     rate = rospy.Rate(100) # 10hz
+    numInt = 50
+    command = 90.
+    lastCommand = 90.
     while not rospy.is_shutdown():
         msg = input('Enter Desired Angle: ')
+        if type(msg) is float or type(msg) is int:
+            lastCommand = command
+            command = msg
+            delta = command-lastCommand
+            interval = delta/numInt
+            for i in range(1,numInt+1):
+                pub.publish(str(lastCommand+interval*i))
         pub.publish(str(msg))
         rate.sleep()
 
