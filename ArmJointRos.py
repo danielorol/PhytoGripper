1afbdbefcbdb8e98525fa88d5bbf3f985e3ebd05
Daniel Orol <danielhorol@gmail.com>
2017-03-30 17:01:19 -0400

    Working PID Controller

--- ArmJointRos.py
+++ ArmJointRos.py
@@ -7,6 +7,7 @@ from tf.transformations import quaternion_from_euler, euler_from_quaternion
 from sensor_msgs.msg import Joy
 from std_msgs.msg import String
 import math, time
+import matplotlib.pyplot as plt
 
 # from std_msgs.srv import Empty
 des_joint_pos = 0.0
@@ -54,30 +55,34 @@ def talker():
     force = rospy.ServiceProxy('/gazebo/apply_joint_effort', ApplyJointEffort)
     joint_state = rospy.ServiceProxy('/gazebo/get_joint_properties', GetJointProperties)
     target_joint_state = rospy.ServiceProxy('/gazebo/set_joint_properties', SetJointProperties)
+    file = open("response.txt", 'w')
     hz = 100
     rate = rospy.Rate(hz)  # 100hz
     error = 0
     lastError = 0
     lastErrorRate = 0
+    totalError = 0
     t = time.clock()
+    k = 1.5
+    grav = -0.25 * offset
+    kd = 0.2
+    ki = 0.05
     while not rospy.is_shutdown():
         if params == 1:
-            k = 1
-            k2 = -0.3
-            kd = 0.15
             alpha = 1
         else:
-            k = 1
-            k2 = 0
-            kd = 0
-            alpha = 0.01
+            alpha = 0.9
         t2 = time.clock()
         joint_pos = joint_state("PhytobiopsyAssembly::Arm_joint").position[0]
         joint_name = "PhytobiopsyAssembly::Arm_joint"
         error = des_joint_pos-joint_pos
-        errorRate = alpha*(error-lastError)/(t2-t)*(1-alpha)*lastErrorRate
-        effort = k*error+k2*offset+kd*errorRate
-        print "Error is " + str(error)
+        errorRate = alpha*(error-lastError)/(t2-t)+(1-alpha)*lastErrorRate
+        effort = k*error+grav+kd*errorRate + ki*totalError
+        print "Joint position is " + str(joint_pos*180./math.pi)
+        print "Error is " + str(error*180./math.pi)
+        print "Effort is " + str(effort)
+        print "Error rate is " + str(errorRate)
+        print "Gravity compensation is " + str(grav)
         print "Done"
         #print effort
         if command == True:
@@ -86,7 +91,15 @@ def talker():
             force(joint_name,0,None,None)
         lastError=error
         lastErrorRate = errorRate
+        if command == True:
+            totalError = totalError + error
         t = t2
+        file.write(str(effort))
+        file.write(',')
+        file.write(str(joint_pos))
+        file.write(',')
+        file.write(str(des_joint_pos))
+        file.write(',')
         rate.sleep()
 
 
